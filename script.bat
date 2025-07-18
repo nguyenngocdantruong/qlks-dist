21bc1011520a694ebb77e92f7f0e668eed26f07f
Nguyen Dan Truong <145260800+nguyenngocdantruong@users.noreply.github.com>
2025-07-19 05:49:43 +0700

    fix ui

--- script.bat
+++ script.bat
@@ -1,3 +1,3 @@
 git pull origin master
 pause
-java --module-path "\lib" --add-modules javafx.controls,javafx.fxml -jar QuanLyKhachSan.jar
+java --module-path lib --add-modules javafx.controls,javafx.fxml -Dprism.order=sw -Dprism.verbose=true -jar QuanLyKhachSan.jar
